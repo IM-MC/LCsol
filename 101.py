0dba3a986acfe53cd9b528384e79209c499a4057
Matthew Chung <mchung02@oath.com>
2018-04-25 18:57:11 -0700

    101

---
+++ 101.py
@@ -0,0 +1,13 @@
+def isSymmetric(root):
+    """
+    :type root: TreeNode
+    :rtype: bool
+    """
+    return root == None or helper(root.left, root.right)
+
+def helper(left, right):
+    if left == None or right == None:
+        return left == right
+    if left.val != right.val:
+        return False
+    return helper(left.left, right.right) and (left.right, right.left)
