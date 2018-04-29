e235cf73d36735cb02b6ac1177bfb54f487c3fcb
im-mc <matthewchung022@hotmail.com>
2018-04-29 16:06:28 -0400

    64

--- 64.py
+++ 64.py
@@ -12,10 +12,10 @@ def minPathSum(grid):
     :rtype: int
     """
 
-    for i in range(1,len(grid)):
+    for i in range(1,len(grid[0])):
         grid[0][i] += grid[0][i-1]
 
-    for i in range(1, len(grid[0])):
+    for i in range(1, len(grid)):
         grid[i][0] += grid[i-1][0]
 
     for i in range(1, len(grid)):
