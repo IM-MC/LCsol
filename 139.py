69e81877a03d295f39a9df16b5e7a89307d3346d
im-mc <matthewchung022@hotmail.com>
2018-05-09 22:29:48 -0400

    139

---
+++ 139.py
@@ -0,0 +1,17 @@
+def wordBreak(s, wordDict):
+    """
+    :type s: str
+    :type wordDict: List[str]
+    :rtype: bool
+    """
+
+    d = [False] * len(s)
+
+    for i in range(len(s)):
+        for w in wordDict:
+            if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
+                d[i] = True
+                
+    return d[-1]
+
+print(wordBreak("aaaa", ["aaaa", "aaa", "a", "and", "cat"]))
