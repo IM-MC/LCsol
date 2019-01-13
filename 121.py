c68cc9f021a4ff24d50cca640b64c2d9a88840bf
Matthew Chung <matthewchung022@hotmail.com>
2019-01-13 11:48:06 -0500

    121

--- 121.py
+++ 121.py
@@ -1,7 +1,22 @@
-def maxProfit(self, prices):
+def maxProfit(prices):
     """
     :type prices: List[int]
     :rtype: int
     """
 
-    
+    if len(prices) == 0:
+        return 0
+
+    min, profit = prices[0] ,0
+
+    for day in range(1, len(prices)):
+        if prices[day] < min:
+            min = prices[day]
+        else:
+            temp = prices[day] - min
+            if temp > profit:
+                profit = temp
+
+    return profit
+
+print(maxProfit([7,1,5,3,6,4]))
