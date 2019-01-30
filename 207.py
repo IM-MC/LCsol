9fbbd600a7f194669cb46c37cda80a71b1c1a762
Matthew Chung <matthewchung022@hotmail.com>
2019-01-30 17:47:55 -0500

    207

---
+++ 207.py
@@ -0,0 +1,34 @@
+def canFinish(numCourses, prerequisites):
+    """
+    :type numCourses: int
+    :type prerequisites: List[List[int]]
+    :rtype: bool
+    """
+
+    dic = [[] for _ in range(numCourses)]
+    visit = [0 for _ in range(numCourses)]
+
+    for x,y in prerequisites:
+        dic[x].append(y)
+
+    def dfs(i):
+        if visit[i] == -1:
+            return False
+        if visit[i] == 1:
+            return True
+
+        visit[i] = -1
+        for node in dic[i]:
+            if not dfs(node):
+                return False
+        visit[i] = 1
+        return True
+
+    for i in range(numCourses):
+        if not dfs(i):
+            return False
+
+    return True
+
+
+print(canFinish(2,[[1,0],[0,1]]))
---
+++ 210.py
@@ -0,0 +1,32 @@
+def findOrder(numCourses, prerequisites):
+    """
+    :type numCourses: int
+    :type prerequisites: List[List[int]]
+    :rtype: List[int]
+    """
+
+    dic = [set() for _ in range(numCourses)]
+    neigh = [set() for _ in range(numCourses)]
+
+    for x,y in prerequisites:
+        dic[x].add(y)
+        neigh[y].add(x)
+
+    stack = [i for i in range(numCourses) if not dic[i]]
+    arr = []
+
+    while stack:
+        node = stack.pop()
+        arr.append(node)
+        for i in neigh[node]:
+            dic[i].remove(node)
+            if not dic[i]:
+                stack.append(i)
+
+    for element in dic:
+        if len(element) != 0:
+            return []
+
+    return arr
+
+print(findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
