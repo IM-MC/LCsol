a5ae090e5cddd0a34e1b359f4c8183874969c283
Matthew Chung <matthewchung022@hotmail.com>
2019-01-31 08:13:39 -0500

    39v2

--- 39.py
+++ 39.py
@@ -7,22 +7,38 @@ def combinationSum(candidates, target):
     array = []
     curr = []
     value = 0
-    backtrack(candidates, target, array, curr, value, 0)
+    backtrack(candidates, target, value, curr, array,0)
     return(array)
 
-def backtrack(candidates, target, array, curr, value, start):
-    if value == target and curr not in array:
-        arrayCopy = list(curr)
-        array.append(arrayCopy)
+# def backtrack(candidates, target, array, curr, value, start):
+#     if value == target and curr not in array:
+#         arrayCopy = list(curr)
+#         array.append(arrayCopy)
+#         return
+#
+#     for i in range(start,len(candidates)):
+#         if value+candidates[i] > target:
+#             continue
+#         curr.append(candidates[i])
+#         value += candidates[i]
+#         backtrack(candidates,target,array,curr,value, i)
+#         del curr[-1]
+#         value -= candidates[i]
+
+
+def backtrack(candidates, target, value, curr, array, start):
+    print(curr)
+    if value == target:
+        listCopy = list(curr)
+        array.append(listCopy)
         return
 
-    for i in range(start,len(candidates)):
-        if value+candidates[i] > target:
+    for i in range(start, len(candidates)):
+        if value + candidates[i] > target:
             continue
-        curr.append(candidates[i])
-        value += candidates[i]
-        backtrack(candidates,target,array,curr,value, i)
-        del curr[-1]
-        value -= candidates[i]
 
-print(combinationSum([2,3,6,7], 7))
+        curr.append(candidates[i])
+        backtrack(candidates, target, value + candidates[i], curr, array,i)
+        del curr[-1]
+
+print(combinationSum([2,3,5], 8))
