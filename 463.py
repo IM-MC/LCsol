7ace42c95a47358a498566ec17f649bddfd79aad
Matthew Chung <matthewchung022@hotmail.com>
2019-02-11 00:43:13 -0500

    463

---
+++ 463.py
@@ -0,0 +1,20 @@
+def islandPerimeter(self, grid):
+    """
+    :type grid: List[List[int]]
+    :rtype: int
+    """
+
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i < len(grid)-1 and grid[i+1][j] == 1:
+                    perimeter -= 2
+
+                if j < len(grid[i])-1 and grid[i][j+1] == 1:
+                    perimeter -= 2
+
+    return perimeter
